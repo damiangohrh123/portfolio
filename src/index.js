3cf3b074bfcbc965e4a5df5f872004d2e75c03ed
damiangohrh123 <damiangohrh@gmail.com>
2024-04-07 20:30:07 +0800

    Add image slideshow

--- src/index.js
+++ src/index.js
@@ -24,25 +24,42 @@ nextBtn.addEventListener("click", () => {
 });
 
 function changeSlides(n) {
-  slideIndex += n;
+  let nextSlideIndex = slideIndex + n;
 
   // Make sure slide index stays within range
-  if (slideIndex >= slides.length) {
-    slideIndex = 0;
+  if (nextSlideIndex >= slides.length) {
+    nextSlideIndex = 0;
   }
-  if (slideIndex < 0) {
-    slideIndex = slides.length - 1;
+  if (nextSlideIndex < 0) {
+    nextSlideIndex = slides.length - 1;
   }
 
-  showSlides();
+  // Pass in direction to showSlide
+  if (n === 1) showSlides("right", slideIndex, nextSlideIndex);
+  if (n === -1) showSlides("left", slideIndex, nextSlideIndex);
+
+  slideIndex = nextSlideIndex;
 }
 
-function showSlides() {
+function showSlides(direction = "none", currentSlideIndex = 0, nextSlideIndex = 0) {
   // Set all images' display to none exept current image
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides[i].classList.remove("slide-in", "slide-in-left", "slide-in-right", "slide-out-left", "slide-out-right");
   }
-  slides[slideIndex].style.display = "block";
+
+  // Add appropriate animation class based on the direction
+  if (direction === "left") {
+    slides[nextSlideIndex].classList.add("slide-in-left");
+    slides[currentSlideIndex].classList.add("slide-out-left");
+  }
+  if (direction === "right") {
+    slides[nextSlideIndex].classList.add("slide-in-right");
+    slides[currentSlideIndex].classList.add("slide-out-right");
+  }
+
+  slides[currentSlideIndex].style.display = "block";
+  slides[nextSlideIndex].style.display = "block";
 }
 
 function startAutoScroll() {
@@ -55,6 +72,7 @@ function stopAutoScroll() {
   clearInterval(autoScrollInterval);
 }
 
-// Initialize auto scrolling 
-startAutoScroll();
+// Initialize auto scrolling
 showSlides();
+startAutoScroll();
+
