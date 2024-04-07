de414760965169cf7d1d72f01e0238c53ad64165
damiangohrh123 <damiangohrh@gmail.com>
2024-04-07 12:40:52 +0800

    Add image slideshow section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 import "./styles.css";
+
+let autoScrollInterval;
+let slideIndex = 0;
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const slides = document.getElementsByClassName("slide");
+
+prevBtn.addEventListener("click", () => {
+  if (slideIndex > 0) {
+    slideIndex--;
+  } else {
+    slideIndex = slides.length - 1;
+  }
+  showSlides();
+  stopAutoScroll();
+  startAutoScroll();
+});
+
+nextBtn.addEventListener("click", () => {
+  if (slideIndex < slides.length - 1) {
+    slideIndex++;
+  } else {
+    slideIndex = 0;
+  }
+  showSlides();
+  stopAutoScroll();
+  startAutoScroll();
+});
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slides[slideIndex].style.display = "block";
+}
+
+function autoScroll() {
+  if (slideIndex < slides.length - 1) {
+    slideIndex++;
+  } else {
+    slideIndex = 0;
+  }
+  showSlides();
+}
+
+function startAutoScroll() {
+  autoScrollInterval = setInterval(autoScroll, 3000);
+}
+
+function stopAutoScroll() {
+  clearInterval(autoScrollInterval);
+}
+
+startAutoScroll();
+showSlides();
